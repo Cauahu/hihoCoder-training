ef5bc3ad5703430aa9e37ef5677cdf839ac3b6f4
Cauahu <1429156553@qq.com>
2016-03-18 14:45:38 +0800

    modify main.cpp

--- 优化延迟（priority_queue）/main.cpp
+++ 优化延迟（priority_queue）/main.cpp
@@ -9,16 +9,16 @@ priority_queue<int> kq;
 bool checkqueue(int size)
 {
     int cur=0;
-    for(int i=0;i<N;i++)
+    for(int i=0;i<N;i++)//将inp数组按题目要求处理
     {
-        if(kq.size()==static_cast<unsigned>(size)&&!kq.empty())
+        if(kq.size()==static_cast<unsigned>(size)&&!kq.empty())//当队列满时，取出最大权值数
         {
             test[cur++]=kq.top();
             kq.pop();
         }
-        kq.push(inp[i]);
+        kq.push(inp[i]);//向队列中添加元素
     }
-    while(!kq.empty())
+    while(!kq.empty())//将队列剩余元素清空
     {
         test[cur++]=kq.top();
         kq.pop();
@@ -26,7 +26,7 @@ bool checkqueue(int size)
     int res=0;
     for(int i=0;i<N;i++)
     {
-        res += (i+1)*test[i];
+        res += (i+1)*test[i];//统计结果
     }
     return res<=Q;
 }
