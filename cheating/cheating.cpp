0699e33d0a71a57265d8181c893b3b8387d34ace
cauahu <andahuzhuang@163.com>
2016-07-06 20:31:49 +0800

    cheating

---
+++ cheating/cheating.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	int n;
+	while(cin >> n){
+		map<int, vector<int> > inmap;
+		int a, m , b;
+		for(int i=0;i<n;++i)
+		{
+			for()
+			{
+				
+			}
+		}
+	}
+
+	return 0;
+}
